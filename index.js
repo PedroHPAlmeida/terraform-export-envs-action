7a4109ed14fd572cbcdb2ffd2aabc9e5e5d4153c
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-04-04 22:27:22 -0300

    fix: bug

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const core = require('@actions/core');
 try {
     const tfVariablesInput = core.getInput('tf_variables');
     const tfVariables = JSON.parse(tfVariablesInput);
-
+   
     Object.keys(tfVariables).forEach(key => {
-        process.env[key] = `TF_${tfVariables[key].toUpperCase()}`;
-        console.log(process.env[`TF_${tfVariables[key].toUpperCase()}`])
+        process.env[`TF_${key}`] = tfVariables[key];
+        console.log(process.env[`TF_${key}`])
     })
 } catch (error) {
     core.setFailed(error.message);
